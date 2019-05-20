6fab37b74fe666cf962e4d20fde0a54935dabfa7
Mustapha Kadri <mustapha.kadri66@gmail.com>
2019-05-20 02:09:54 +0200

    feat (storage) : add LocalStorage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,25 @@ class App extends Component {
   state = {
     text : sampleText
   }
+  // Cycle de vie du component 
+  componentDidMount() {
+    //console.log('je suis monté')
+    // on recupere le contenu stocké à la clé "text"
+    const text = localStorage.getItem('text')
+    if (text){
+      this.setState({text})
+    } else {
+      this.setState({text: sampleText})
+    }
+    
+  }
 
+  componentDidUpdate(){
+    //console.log('je suis mis à jour')
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+  
   handleChange = (event) => {
     // on recupere la valeur de l'input
     const text = event.target.value
@@ -30,14 +48,12 @@ class App extends Component {
               onChange={this.handleChange}
               value={this.state.text}
               className="form-control"
-              rows="35">
-               
+              rows="35"> 
               </textarea>
             </div>
             <div className="col-sm-6">
               <div>
                 <div dangerouslySetInnerHTML={this.renderText(this.state.text)}>
-
                 </div>
               </div>
             </div>
